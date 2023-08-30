31f77dc964c3c79c1a020cd9dae37a463aa4ece6
Sriakshaj-D <123798653+Sriakshaj-D@users.noreply.github.com>
2023-08-30 17:32:40 +0400

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,5 @@
+function send(){
+    number1 = document.getElementById("number1_input").value;
+    number2 = document.getElementById("number2_input").value;
+    actual_answer = parseInt(number1)*parseInt(number2);
+}
